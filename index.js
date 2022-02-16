02531f3e47afb26a3f4e45feb335ff2ba0220864
NEHA KUMARI <neha.silicon@gmail.com>
2022-02-16 11:59:52 -0500

    Completed task3

--- index.js
+++ index.js
@@ -209,16 +209,21 @@ Practice accessing data above by console.log-ing following items:
 
 //(1) Name of the first artist (0th index) in the array
 
-
+console.log(artists[0].name)
 //(2) Bio of the third artist (2nd index) in the array 
+console.log(artists[2].bio)
 
-
-
+console.log('%%%%%%%%%%%%%%%%% *5')
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 (no function needed) 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Fix this issue and console.log() to check your work. */
-
-
+for(let i=0; i<artists.length; i++){
+  if (artists[i].name ==="Vincent van Dough"){
+    artists[i].name = "Vincent Van Gough"
+    console.log(`${artists[i].name} at index ${i}`)
+  }
+}
+console.log(artists[8].name)
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀  
 Use getArtistByIndex to do the following:
@@ -228,8 +233,10 @@ Use getArtistByIndex to do the following:
 
 Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
+function getArtistByIndex(arr,index) {
   /*Your Code Here*/
+  return  `the artist at index ${arr[index].id} is ${arr[index].name}`
+
 }
 
 
